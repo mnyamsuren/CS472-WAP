a6280c6e199d5e181e2086ae64e39e50b28ea71c
munkhdalai05 <n.munkhdalai@gmail.com>
2021-06-08 13:46:17 -0500

    Lab9

--- Lab9/nodeCalc.js
+++ Lab9/nodeCalc.js
@@ -1,14 +1,14 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var calcMod = require('./nodeCalcModule.js');
+var calcModule = require('./nodeCalcModule.js');
 
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
 
     if (q.pathname=="/nodeCalc.js")
-        calcMod.calc(req,res,q.query)
+        calcModule.calc(req,res,q.query)
     else
         fs.readFile(filename, function(err, data) {
             if (err) {
@@ -17,6 +17,7 @@ http.createServer(function(req, res){
             }
             res.writeHead(200); // Content-Type not included
             res.write(data);
+            // res.write('nodeCalc.html');
             return res.end();
         });
 }).listen(8080);
--- Lab9/nodeCalcModule.js
+++ Lab9/nodeCalcModule.js
@@ -2,7 +2,9 @@ const { Console } = require("console");
 
 exports.calc = function (req,res,vals) {
     var answer = 0;
-    console.log(vals.operation);
+    // console.log(vals.num1);
+    // console.log(vals.operation);
+    // console.log(vals.num2);
     switch(vals.operation){
         case '+': 
         answer = parseInt(vals.num1) + parseInt(vals.num2);
@@ -22,41 +24,16 @@ exports.calc = function (req,res,vals) {
         default: break;
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
-    es.write("<form action = 'http://localhost:8080/nodeCalc.js' method= 'get'>");
-    // res.write("<!DOCTYPE html>");
-    // res.write("<html>");
+    res.write("<!DOCTYPE html>");
+    res.write("<html>");
     res.write("<head><meta charset=\"utf-8\"/>");
     res.write("<title>Calculator</title>");
     res.write("</head>");
     res.write("<body>");
-    res.write("<h1> Simple Calculator</h1>");
-    //res.write("<img src="calc.jpg" alt="calculator image" />");
-    res.write("<div>");
-    res.write("<label> First Number:   </label><input class= 'input' id='num1' type='number' maxlength='9'>");
-    res.write("<p></p>");
-    res.write("</div>");
-
-    res.write("<div>");
-    res.write("<select id='operator'>");
-    res.write("<option value='+''>+</option>");
-    res.write("<option value='-'>-</option>");
-    res.write("<option value='/'>/</option>");
-    res.write("<option value='*'>*</option>");
-    res.write("</select>");
-    res.write("<p></p>");
-    res.write("</div>");
-
-    res.write("<div>");
-    res.write("<label> Second Number:   </label><input class= 'input' id='num2' type='number' maxlength='9'>");
-    res.write("<p></p>");
-    res.write("</div>");
-    res.write("<input type='submit'>");
-    res.write("<p></p>");
-    res.write("<p font-size: large> The answer is: ");
+    res.write("<h3>Result: ");
     res.write(String(answer));
-    res.write("</p>");
+    res.write("</h3>");
     res.write("</body>");
-    // res.write("</html>");
-    res.write("</form>");
+    res.write("</html>");
     return res.end();
-}
+};
